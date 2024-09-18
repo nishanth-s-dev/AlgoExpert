b46828ad0eaec98320585260c4f7c474813e8a79
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-18 23:37:46 +0530

    problem - transpose_matrix

--- easy/transpose_matrix.py
+++ easy/transpose_matrix.py
@@ -1,4 +1,4 @@
-def transposeMatrix(matrix):
+def transposeMatrixMethodOne(matrix):
     height = len(matrix)
     width = len(matrix[0])
 
@@ -8,4 +8,14 @@ def transposeMatrix(matrix):
         for col in range(width):
             res[col][row] = matrix[row][col]
 
+    return res
+
+def transposeMatrixMethodTwo(matrix):
+    # Write your code here.
+    res = []
+    for col in range(len(matrix[0])):
+        newRow = []
+        for row in range(len(matrix)):
+            newRow.append(matrix[row][col])
+        res.append(newRow)
     return res
