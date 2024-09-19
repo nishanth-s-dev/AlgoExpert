a3c08fb0487bd76302b7a1c99763cb4ed8282d53
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-19 22:44:06 +0530

    Group palindromes into a list and return it.

---
+++ easy/semordilap.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+
+def semordnilap(words):
+    hs = set()
+    semordnilaps = []
+
+    for word in words:
+        reversed_word = word[::-1]
+        if reversed_word in hs:
+            semordnilaps.append((word, reversed_word))
+        hs.add(word)
+
+    return semordnilaps
