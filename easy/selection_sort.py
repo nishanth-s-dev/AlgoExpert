37caa9d3eb4d1c13005986ac1f221251f8fa3735
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-21 10:39:30 +0530

    Selection Sort

---
+++ easy/selection_sort.py
@@ -0,0 +1,8 @@
+def selectionSort(array):
+    for i in range(len(array)):
+        minIdx = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minIdx]:
+                minIdx = j
+        array[i], array[minIdx] = array[minIdx], array[i]
+    return array
