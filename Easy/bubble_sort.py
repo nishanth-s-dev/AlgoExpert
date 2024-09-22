d3dba20e4236119e7651f02cd7d3009bddafa084
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-22 05:57:21 +0530

    bubble-sort description update

--- Easy/bubble_sort.py
+++
@@ -1,6 +0,0 @@
-def bubbleSort(array):
-    for i in range(len(array)):
-        for j in range(len(array) - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-    return array
