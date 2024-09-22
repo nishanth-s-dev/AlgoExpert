5c4c09e8258276a665f5fa506458849c075cf724
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-22 06:36:43 +0530

    product-sum description update

--- Easy/product_sum.py
+++
@@ -1,8 +0,0 @@
-def productSum(array, depth = 1):
-    sum = 0
-    for item in array:
-        if type(item) == list:
-            sum += productSum(item, depth + 1)
-        else:
-            sum += item
-    return sum * depth
