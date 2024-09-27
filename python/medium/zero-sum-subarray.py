e2c928044721c13ff2804958c771dc91870ff42c
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-27 22:19:00 +0530

    remove duplicate code

--- python/medium/zero-sum-subarray.py
+++ python/medium/zero-sum-subarray.py
@@ -1,36 +1,5 @@
 # Problem : https://www.algoexpert.io/questions/zero-sum-subarray
 
-def arrayOfProducts(array):
-    """
-    Thought Process:
-    The goal is to create a new array where each element at index `i` contains the product of all the numbers in the input array except the one at `i`.
-
-    Approach:
-    1. This brute-force approach involves computing the product of every other element in the array except for the current index.
-    2. For each element `i` in the array, initialize a `product` variable to 1.
-    3. Loop through the array again (inner loop) and multiply the `product` by all elements except for the one at the current index `i`.
-    4. Append the result to the `res` array.
-    5. Return the `res` array, which will contain the product of all elements except the one at the current index for each position.
-
-    This solution, while simple, has a time complexity of O(n^2), where `n` is the length of the array, since we have nested loops.
-    This is inefficient for larger arrays because of the repeated multiplication operations.
-
-    Time Complexity: O(n^2) due to the nested loops.
-    Space Complexity: O(n) for storing the result array.
-    """
-
-    res = []
-
-    for i in range(len(array)):
-        product = 1
-        for j in range(len(array)):
-            if j != i:
-                product *= array[j]
-        res.append(product)
-
-    return res
-
-
 def zeroSumSubarray(nums):
     """
     Thought Process:
