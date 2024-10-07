1c91970954802322a2f9e268c8950c2d93e9c589
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-10-08 01:35:11 +0530

    binary-tree method implemented : insert, __str__

--- python/data-structure/binary-tree.py
+++ python/data-structure/binary-tree.py
@@ -1,14 +1,34 @@
+from collections import deque
+
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.left, self.right = None, None
+
+    def __str__(self):
+        return f"Value: {self.value}"
 
 class BinaryTree:
     def __init__(self, value):
         self.root = Node(value)
 
     def insert(self, value):
-        pass
+        new_node = Node(value)
+        queue = [self.root]
+        while queue:
+            current_node = queue.pop(0)
+            if current_node.left is None:
+                current_node.left = new_node
+                break
+            else:
+                queue.append(current_node.left)
+
+            if current_node.right is None:
+                current_node.right = new_node
+                break
+            else:
+                queue.append(current_node.right)
 
     def delete(self, value):
         pass
@@ -62,6 +82,24 @@ class BinaryTree:
     def lowest_common_ancestor(self):
         pass
 
+    # Level order
+    def __str__(self):
+        res = []
+        queue = deque([self.root])
+        while queue:
+            current = queue.popleft()
+            res.append(str(current.value))
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+        return " ".join(res)
 
 if __name__ == '__main__':
-    print("Binary Tree")
+    binary_tree = BinaryTree(1)
+    binary_tree.insert(2)
+    binary_tree.insert(3)
+    binary_tree.insert(4)
+    binary_tree.insert(5)
+    binary_tree.insert(6)
+    print(binary_tree)
