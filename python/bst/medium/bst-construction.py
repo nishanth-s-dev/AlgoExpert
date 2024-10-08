96f803f6dc060f6eb80eee9077f48838d9bce9b7
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-10-09 04:37:53 +0530

    bst-construction solution added. Need to revise delete method

--- python/bst/medium/bst-construction.py
+++ python/bst/medium/bst-construction.py
@@ -39,5 +39,41 @@ class BST:
                 else:
                     current = current.right
 
-    def delete(self, value):
-        pass
+    # Need to revise this
+    def remove(self, value, parent_node = None):
+        current_node = self
+        while current_node:
+            if value < current_node.value:
+                parent_node = current_node
+                current_node = current_node.left
+            elif value > current_node.value:
+                parent_node = current_node
+                current_node = current_node.right
+            else:
+                if current_node.left is not None and current_node.right is not None:
+                    current_node.value = current_node.right.get_min_value()
+                    current_node.right.remove(current_node.value, current_node)
+                elif parent_node is None:
+                    if current_node.left is not None:
+                        current_node.value = current_node.left.value
+                        current_node.right = current_node.left.right
+                        current_node.left = current_node.left.left
+                    elif current_node.right is not None:
+                        current_node.value = current_node.right.value
+                        current_node.left = current_node.right.left
+                        current_node.right = current_node.right.right
+                    else:
+                        # This is a single-node tree; do nothing.
+                        pass
+                elif parent_node.left == current_node:
+                    parent_node.left = current_node.left if current_node.left is not None else current_node.right
+                elif parent_node.right == current_node:
+                    parent_node.right = current_node.left if current_node.left is not None else current_node.right
+                break
+        return self
+
+    def get_min_value(self):
+        current_node = self
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
