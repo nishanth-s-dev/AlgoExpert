81d4c62be574c2f4ce063a75ab78931b0572c33f
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-10-26 19:44:38 +0530

    split-binary-tree dfs solution added

--- python/binary-tree/medium/split-binary-tree.py
+++ python/binary-tree/medium/split-binary-tree.py
@@ -10,6 +10,7 @@ class BinaryTree:
         self.right = right
 
 
+# O(n) Time | O(n) Space
 def splitBinaryTree(tree):
     if tree is None:
         return 0
@@ -38,6 +39,33 @@ def splitBinaryTree(tree):
 
     return 0
 
+
+# O(n) Time | O(n) Space
+# DFS Method
+def splitBinaryTree(tree):
+    if tree is None:
+        return 0
+
+    memo = dict()
+
+    totalSum = getSum(tree)
+    if totalSum % 2 == 0:
+        if dfsHelper(tree, totalSum // 2, memo):
+            return totalSum // 2
+
+    return 0
+
+
+def dfsHelper(node, expectedSum, memo):
+    if node is None:
+        return False
+
+    if node not in memo:
+        memo[node] = getSum(node)
+
+    currentSum = memo[node]
+
+    return currentSum == expectedSum or dfsHelper(node.left, expectedSum, memo) or dfsHelper(node.right, expectedSum, memo)
 
 def getSum(node):
     if node is None:
